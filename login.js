1fd86914c1823ff119b81b3c7d4acccdfb3edc09
Albin K P <118466308+Albin-kp@users.noreply.github.com>
2023-06-16 19:52:59 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,18 @@
-function logincheck()
-{
-var username = document.getElementById("username").value
-var password = document.getElementById("password").value
-if (username=="albinkp"&&password=="8266") {
-    window.location.href="control.html"
-} else {
-    document.getElementById("message").textContent="incorrect username or password"
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
     
-}
-}
+    // Get the entered username and password
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    // Perform username and password validation
+    if (username === "albinkp" && password === "albinkp") {
+      // Redirect to another file
+      window.location.href = "switch.html";
+    } else {
+        window.location.href="login.html"
+      alert("Invalid username or password. Please try again.");
+      document.getElementById("message").textContent="invalid username or password"
+    }
+  });
+  
